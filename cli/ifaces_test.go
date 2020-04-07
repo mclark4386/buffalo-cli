011ed88d996851bef002d013afe7c4ae2ce0f783
Matthew Clark <484943+mclark4386@users.noreply.github.com>
2020-04-07 00:55:48 -0500

    *filled out the cli/main tests

---
+++ cli/ifaces_test.go
@@ -0,0 +1,82 @@
+package cli
+
+import (
+	"io"
+	"testing"
+
+	"github.com/gobuffalo/plugins"
+	"github.com/stretchr/testify/require"
+)
+
+type TestPlugin struct{}
+
+func (t TestPlugin) PluginName() string {
+	return "test/plugin"
+}
+
+func Feeder_Test() []plugins.Plugin {
+	return []plugins.Plugin{
+		TestPlugin{},
+	}
+}
+
+type TestNeeder struct {
+	Testy               *testing.T
+	ExpectedPluginCount int
+}
+
+func (tn TestNeeder) PluginName() string {
+	return "test/plugin/needer"
+}
+
+func (tn TestNeeder) WithPlugins(feeder plugins.Feeder) {
+	r := require.New(tn.Testy)
+	ps := feeder()
+	r.Equal(len(ps), tn.ExpectedPluginCount)
+}
+
+func Test_TestNeeder(t *testing.T) {
+
+	tn := TestNeeder{
+		Testy:               t,
+		ExpectedPluginCount: 1,
+	}
+
+	tn.WithPlugins(Feeder_Test)
+}
+
+type TestInNeeder struct {
+	Error error
+}
+
+func (tin TestInNeeder) PluginName() string {
+	return "test/plugin/inneeder"
+}
+
+func (tin TestInNeeder) SetStdin(io.Reader) error {
+	return tin.Error
+}
+
+type TestOutNeeder struct {
+	Error error
+}
+
+func (ton TestOutNeeder) PluginName() string {
+	return "test/plugin/outneeder"
+}
+
+func (ton TestOutNeeder) SetStdout(io.Writer) error {
+	return ton.Error
+}
+
+type TestErrNeeder struct {
+	Error error
+}
+
+func (ten TestErrNeeder) PluginName() string {
+	return "test/plugin/errneeder"
+}
+
+func (ten TestErrNeeder) SetStderr(io.Writer) error {
+	return ten.Error
+}
--- cli/main_test.go
+++ cli/main_test.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"bytes"
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/gobuffalo/plugins"
@@ -18,6 +19,13 @@ func Test_Buffalo_Help(t *testing.T) {
 	b := &Buffalo{
 		Plugins: plugins.Plugins{
 			plugio.NewOuter(stdout),
+			TestNeeder{
+				Testy:               t,
+				ExpectedPluginCount: 5,
+			},
+			TestOutNeeder{},
+			TestInNeeder{},
+			TestErrNeeder{},
 		},
 	}
 
@@ -29,6 +37,56 @@ func Test_Buffalo_Help(t *testing.T) {
 	r.NoError(err)
 
 	r.Contains(stdout.String(), b.Description())
+}
+
+func Test_Buffalo_FailingPlugins(t *testing.T) {
+	r := require.New(t)
+
+	stdout := &bytes.Buffer{}
+
+	//Bad Out
+	b := &Buffalo{
+		Plugins: plugins.Plugins{
+			plugio.NewOuter(stdout),
+			TestNeeder{
+				Testy:               t,
+				ExpectedPluginCount: 5,
+			},
+			TestOutNeeder{Error: errors.New("Bad Out")},
+			TestInNeeder{},
+			TestErrNeeder{},
+		},
+	}
+
+	ctx := context.Background()
+
+	args := []string{"-h"}
+
+	err := b.Main(ctx, "", args)
+	r.Error(err)
+	r.Equal("Bad Out", err.Error())
+
+	//Bad In
+	b = &Buffalo{
+		Plugins: plugins.Plugins{
+			TestInNeeder{Error: errors.New("Bad In")},
+		},
+	}
+
+	err = b.Main(ctx, "", args)
+	r.Error(err)
+	r.Equal("Bad In", err.Error())
+
+	//Bad Err
+	b = &Buffalo{
+		Plugins: plugins.Plugins{
+			TestErrNeeder{Error: errors.New("Bad Err")},
+		},
+	}
+
+	err = b.Main(ctx, "", args)
+	r.Error(err)
+	r.Equal("Bad Err", err.Error())
 }
 
 func Test_Buffalo_Main_SubCommand(t *testing.T) {
@@ -73,4 +131,20 @@ func Test_Buffalo_Main_SubCommand_Alias(t *testing.T) {
 	err := b.Main(ctx, "", args)
 	r.NoError(err)
 	r.Equal(exp, c.args)
+}
+
+func Test_Buffalo_WithoutPlugins(t *testing.T) {
+	r := require.New(t)
+
+	b := &Buffalo{}
+
+	ctx := context.Background()
+
+	args := []string{"sc"}
+
+	exp := []string{"hello"}
+	args = append(args, exp...)
+
+	err := b.Main(ctx, "", args)
+	r.NoError(err)
 }
